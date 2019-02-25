b2344b83062fc7877a5b022c20ce799252ea77b0
martensievers <hello@martensievers.com>
2019-02-25 19:10:23 +0100

    new functions and arrays file

---
+++ functions-and-arrays/notes-app.js
@@ -0,0 +1,19 @@
+/**
+* Notes-App: Take notes like never before
+*/
+
+
+// Initialize new array of notes
+var notes = [{
+    title: "Plan next vacation",
+    body: "Book flights to Madrid"
+}, {
+    title: "Buy Breakfast",
+    body: "Milk, Cereal & Fruits"
+}, {
+    title: "Walk the dog",
+    body: "Don't forget to bring a leash"
+}, {
+    title: "Upgrade the Office",
+    body: "Buy a new office chair"
+}]
---
+++ functions-and-arrays/todo-app.js
@@ -0,0 +1,16 @@
+var todos = [{
+    title: 'Walk the dog',
+    completed: false
+}, {
+    title: 'Clean my room',
+    completed: false
+}, {
+    title: 'Read a book',
+    completed: true
+}, {
+    title: 'Buy food',
+    completed: false
+}, {
+    title: 'Book a flight',
+    completed: true
+}]
